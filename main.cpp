b79d0c1e41a9629553319b666f75ff9b284314d2
seyi-gh <aesioseyuri@gmail.com>
2026-02-09 16:18:13 -0500

    feat: core for implementing the load of the data

--- main.cpp
+++ main.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
+#include <ctime>
 #include "source/Table.h"
 using namespace std;
 
-#define PLAYER1 1
-#define PLAYER2 2
-
 int main() {
+  srand(static_cast<unsigned int>(time(nullptr)));
+  
   Table table = Table();
 
-  table.place_turn(PLAYER1, 1, 0);
+  int player1 = 1;
+  int player2 = 2;
+  int player_turn = player1;
 
-  table.print();
+  cout << "> Starting test <" << endl;
+
+  while (true) {
+    if (table.posible_places.size() == 0) {
+      cout << "There is no winner in this run" << endl;
+      break;
+    }
+    vector<int> next_position = table.get_random_position();
+    bool response = table.place_turn(next_position[0], next_position[1], player_turn);
+
+    table.print();
+    cout << "Row: " << next_position[0] << " Column: " << next_position[1] << endl;
+    cout << "Response place in the table: " << response << endl << endl;
+
+    bool winner_checker = table.check_placement_winner(next_position[0], next_position[1]);
+    if (winner_checker) {
+      cout << "The winner is: " << player_turn << endl;
+      break;
+    }
+
+    if (player_turn == player1)
+      player_turn = player2;
+    else player_turn = player1;
+  }
+
+  cout << "> Ending test <" << endl;
 
   return 0;
 }
--- source/Table.h
+++ source/Table.h
@@ -2,33 +2,90 @@
 
 #include <iostream>
 #include <vector>
+#include <cstring>
+#include <random>
+#include <chrono>
 using namespace std;
 
 class Table {
 public:
   int height, width;  
+  
+  vector<string> posible_places;
   vector<vector<int>> table;
 
   Table() {
     height = 3;
     width = 3;
 
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        string temp_position = { _convert_char(i), _convert_char(j) };
+        posible_places.push_back(temp_position);
+      }
+    }
+
     table = vector<vector<int>>(height, vector<int>(width, 0));
+
   }
 
-  void place_turn(int player, int x, int y) {
-    //TODO Check this to more reayaility
-    table[y][x] = player;
+  vector<int> get_random_position() {
+    int random_choice = rand() % posible_places.size();
+    vector<int> ret_temp;
+    ret_temp = {
+      _convert_int(posible_places[random_choice][0]),
+      _convert_int(posible_places[random_choice][1])
+    };
+    posible_places.erase(posible_places.begin() + random_choice);
+    return ret_temp;
   }
 
-  void check_placement_winner(int x, int y) {
-    int player = table[y][x];
-
-    // Check column
+  char _convert_char(int &number) {
+    return (char)(number + '0');
+  }
+  int _convert_int(char &letter) {
+    return (int)(letter - '0');
+  }
+  bool check_place_available(int &row, int &column) {
+    return table[row][column] == 0;
+  }
+  bool place_turn(int row, int column, int player) {
+    if (!check_place_available(row, column)) return false;
     
-    // Check row
-    // Check diagonal
+    table[row][column] = player;
 
+    return true;
+  }
+
+  bool check_placement_winner(int row, int column) {
+    //? Just remember the x is the columns and the y is the rows
+    int player = table[row][column];
+    bool checker = false;
+
+    //? Check the colums
+    if ((table[row][0] == table[row][1]) && (table[row][1] == table[row][2])) {
+      cout << "row" << endl;
+      return true;
+    }
+
+    //? Check the rows
+    if ((table[0][column] == table[1][column]) && (table[1][column] == table[2][column])) {
+      cout << "column" << endl;
+      return true;
+    }
+
+    //? Check the diagonals
+    //TODO Check the big o times to not always check the diagonals
+    if (
+      table[0][0] == player &&
+      table[1][1] == player &&
+      table[2][2] == player
+    ) {
+      cout << "diagonal" << endl;
+      return true;
+    }
+
+    return false;
   }
 
   void print() {
